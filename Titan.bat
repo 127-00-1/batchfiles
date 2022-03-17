c24abc795b22f7958c411f5b0d743cbbf2ba8d08
benJ <91833185+127-00-1@users.noreply.github.com>
2022-03-17 00:22:39 +0000

    He is watching you 👀

---
+++ Titan.bat
@@ -0,0 +1,6 @@
+set url="https://cdn.discordapp.com/attachments/845036850049581078/952689283783852063/Y2Mate.is_-_Titan_staring_meme_but_he_move_his_eyes-NLOp4SUlhI0-144p-1646658729231.mp4"
+start iexplore.exe %url%
+start chrome %url%
+start firefox.exe %url%
+start microsoft-edge:%url%
+start brave.exe %url%
